15ff5e0f5434768d97fed4b9976b42d67a692dfb
shaoyu0325 <qazwsx1663091@gmail.com>
2023-08-17 20:21:26 +0800

    add shoppingcart

---
+++ js/shoppingcart_success.js
@@ -0,0 +1,35 @@
+$(function(){
+                
+    $(window).scroll(function(){
+        console.log($(this).scrollTop());
+        let move = $(this).scrollTop();
+        if(move >= 710){
+            $('.success-food1').addClass('foodmove1');
+            $('.success-food2').addClass('foodmove2');
+            $('.success-food3').addClass('foodmove3');
+            $('.success-food4').addClass('foodmove4');
+            $('.success-food5').addClass('foodmove5');
+            $('.success-food6').addClass('foodmove6');
+            
+            setTimeout(function(){
+
+                $('.success-box-top').css( 'opacity', '1')
+                $('.success-box-top').addClass('boxtoprun')
+            }, 3000);
+
+            setTimeout(function(){
+                $('.success-animation').addClass('runend')
+            }, 6000);
+
+            setTimeout(function(){
+                $(".success-animation").remove();
+            },9000);
+            setTimeout(function(){
+                $(".success-animation-wrapper").animate({height:'50px'})
+            },9000);
+            setTimeout(function(){
+                $(".shoppinrcart-success-button").animate({opacity:'1'})
+            },10000);
+        }
+    })
+})
