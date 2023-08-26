e8e1420f080d6bbf43496abd641ce75af633eee2
Edward8246 <winwinwin8246@gmail.com>
2023-08-26 23:05:39 +0800

    member

--- API/Frontend/Member.php
+++ API/Frontend/Member.php
@@ -1,7 +1,29 @@
 <?php
- include("../Lib/Util.php");	   
+//  include("../Lib/Util.php");	   
 
-	//顯示會員資訊
-    $str = getMemberName();
-	echo $str == "" ? "<a href='index.html'>登入</a>" : "Hello~".$str."&nbsp;&nbsp;&nbsp;<a href='API/Logout.php'>登出</a>";
+// 	//顯示會員資訊
+//     $str = getMemberName();
+// 	echo $str == "" ? "<a href='index.html'>登入</a>" : "Hello~".$str."&nbsp;&nbsp;&nbsp;<a href='API/Logout.php'>登出</a>";
+
+
+
+	include("../Lib/Util.php");
+
+	// 顯示登入或登出按鈕
+	if (getMemberName() == "") {
+		echo "<div class='btn_white user login' id='user_login'>";
+		echo "    <i class='fa-solid fa-user'></i>";
+		echo "    <h7><a href='index.html'>登入</a></h7>";
+		echo "</div>";
+	} else {
+		echo "<div class='btn_white user logout -none' id='user_logout'>";
+		echo "    <i class='fa-solid fa-arrow-right-from-bracket'></i>";
+		echo "    <h7>Hello~" . getMemberName() . "&nbsp;&nbsp;&nbsp;<a href='API/Logout.php'>登出</a></h7>";
+		echo "</div>";
+	}
+
+
+
+
+
 ?>
