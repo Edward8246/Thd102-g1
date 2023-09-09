c726f0aecc3d79402ced97336b03c1e27476776c
MaegiW <maggie90228@gmail.com>
2023-09-09 11:46:33 +0800

    modify member add member JS

--- js/member.js
+++ js/member.js
@@ -34,30 +34,112 @@ tab_h.addEventListener("click", function () {
 
 });
 
-function initial() {
-    const GetData = localStorage.getItem('user');
+// function initial() {
+//     const GetData = localStorage.getItem('user');
   
-    fetch(`/thd102/g1/API/Frontend/setting.php?account=${GetData}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-      .then(response => response.json())
-      .then(data => {
+//     fetch(`/thd102/g1/API/Frontend/setting.php?account=${GetData}`, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         document.getElementById('name').value = data[0].name;
+//         document.getElementById('email').value = data[0].email;
+//         document.getElementById('password').value = data[0].password;
+//         document.getElementById('address').value = data[0].address;
+//         document.getElementById('phone').value = data[0].phone;
+
+            
+//             // name = data.name;
+//         console.log(data); // 打印响应数据到控制台
+//       })
+//       .catch(error => {
+//         console.error(error); // 打印错误信息到控制台
+//       });
+//   }
+  
+//   initial();
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // 當文檔載入完成後執行
+  
+    // 初始化表單
+    initial();
+  
+    // 添加保存更改的事件監聽器
+    document.getElementById('saveChanges').addEventListener('click', function() {
+        saveChanges();
+    });
+  });
+  
+  
+    function initial() {
+      const GetData = localStorage.getItem('user');
+      fetch("/thd102/g1/API/Frontend/setting.php?account=".concat(GetData), {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(function (response) {
+        return response.json();
+      }).then(function (data) {
         document.getElementById('name').value = data[0].name;
         document.getElementById('email').value = data[0].email;
         document.getElementById('password').value = data[0].password;
         document.getElementById('address').value = data[0].address;
         document.getElementById('phone').value = data[0].phone;
-
-            
-            // name = data.name;
+    
+        // name = data.name;
         console.log(data); // 打印响应数据到控制台
+      })["catch"](function (error) {
+        console.error(error); // 打印错误信息到控制台
+      });
+    }
+    
+    initial();
+  
+  function saveChanges() {
+    // 收集表單資料
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
+  
+    // 封裝成 JSON 格式
+    const account = localStorage.getItem('user'); // 假設您已經在 localStorage 設置了用戶資料
+    const userData = {
+        "name": name,
+        "phone": phone,
+        "password": password,
+        "email": email,
+        "address": address
+    };
+    const payload = {
+        "account": account,
+        "userData": userData
+    };
+  
+    // 發送更新請求到後端
+    fetch("/thd102/g1/API/Frontend/setting_update.php", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    }).then(response => response.json())
+      .then(data => {
+          console.log(data);
+          // 可以在這裡加入成功的反饋，例如：
+          if (data.message === "更新成功") {
+              alert("更新成功！");
+          }
       })
       .catch(error => {
-        console.error(error); // 打印错误信息到控制台
+          console.error(error);
+          // 可以在這裡加入失敗的反饋
       });
   }
   
-  initial();
