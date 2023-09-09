df44d1c0af77e3f9f9048878e0bbd0823cbf5ea0
Edward8246 <winwinwin8246@gmail.com>
2023-09-09 15:54:47 +0800

    歷史訂單01

--- API/Frontend/OrderHistory.php
+++ API/Frontend/OrderHistory.php
@@ -1,18 +1,22 @@
 <?php    
+	include("../Lib/Member.php");
     include("../Lib/Util.php");
-    include("../Lib/Member.php");
 
     //建立SQL
-    $sql = "SELECT orders.id,orders.created_date, customers.email, customers.name,SUM(unit_price*quantity) AS total_price
+    $sql = "SELECT orders.created_date,orders.orders_num,SUM(unit_price*quantity) AS total_price
     FROM orders 
     left JOIN customers on orders.customer_id = customers.id 
     left JOIN order_detail on orders.id = order_detail.order_id
     left JOIN products on order_detail.product_id = products.id
+    WHERE customer_id = ? 
     GROUP BY orders.id
-    ORDER BY orders.customer_id ASC";
-    
+    ORDER BY orders.created_date DESC";
+        
     //執行
     $statement = getPDO()->prepare($sql);
+
+    //給值
+    $statement->bindValue(1 , getMemberID()); 
     $statement->execute();
     $data = $statement->fetchAll();
 
---
+++ js/Orderhistory.js
@@ -0,0 +1,32 @@
+
+
+//取得訂單資訊
+function getData(){    
+    $.ajax({            
+        method: "POST",
+        url: "API/Frontend/OrderHistory.php",
+        data:{},
+        dataType: "json",
+        success: function (response) {
+            display(response);
+        },
+        error: function(exception) {
+            alert("數據載入失敗: " + exception.status);
+        }
+    });
+}
+getData();
+
+//顯示訂單資訊
+function display(response){
+    //更新html內容(透過jQuery跑迴圈取值)
+    $.each(response, function(index, row) {
+        $("#table_imfo").append(
+            "<tr>" +
+                "<td><a href='OrderDetail.html?OID=" + row.id + "'>" + row.orders_num + "</td>" +
+                "<td>" + row.created_date + "</td>" +
+                "<td>" + row.total_price + "</td>" +
+            "</tr>"
+        );  
+    });
+}
